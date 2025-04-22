2b5dddcfba9093389c90dddab628b7761765853b
vijitha2112 <vijitha8283@gmail.com>
2025-04-22 16:30:51 +0530

    Adding admin file

--- Ams_be/Ams_app/admin.py
+++ Ams_be/Ams_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Employee, LeaveRequest
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'role', 'manager')
+    list_filter = ('role',)
+    search_fields = ('user__username', 'role')
+
+@admin.register(LeaveRequest)
+class LeaveRequestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'employee', 'leave_type', 'start_date', 'end_date', 'status', 'approved_by', 'applied_at')
+    list_filter = ('leave_type', 'status')
+    search_fields = ('employee__user__username', 'reason')
+    readonly_fields = ('applied_at',)
