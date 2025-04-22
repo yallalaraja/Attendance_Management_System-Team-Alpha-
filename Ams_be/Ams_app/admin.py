76c011c367373d6a20ecc5ee4e30f4475881406f
Kranthi Atla <Kranthiatla95@gmail.com>
2025-04-22 16:37:53 +0530

    Adding admin file

--- Ams_be/Ams_app/admin.py
+++ Ams_be/Ams_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'name', 'role', 'is_active', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'role', 'password1', 'password2'),
+        }),
+    )
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(User, UserAdmin)
