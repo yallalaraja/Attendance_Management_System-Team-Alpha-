d0d62f18ea1089f301738d4a065264f20ee4d75c
srikant <srikanthpatil6868@gmail.com>
2025-04-22 16:46:11 +0530

    Admin panel for the attendance module created

--- Ams_be/Ams_app/admin.py
+++ Ams_be/Ams_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Attendance
 
-# Register your models here.
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'status', 'check_in', 'check_out')
+    list_filter = ('status', 'date')
+    search_fields = ('user__username', 'user__email', 'date')
+    ordering = ('-date',)
+    readonly_fields = ('user', 'date')
+
+    # Optional: This will prevent editing of user/date after creation from the admin
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing an existing object
+            return self.readonly_fields + ('check_in', 'check_out', 'status')
+        return self.readonly_fields
