1573fc1aab2a6d932f16b6f20e36f5ea5db41ff9
raja reddy <121497045+yallalaraja@users.noreply.github.com>
2025-04-22 18:08:50 +0530

    Update urls.py

--- Ams_be/Ams_app/urls.py
+++ Ams_be/Ams_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views  # or specific views
+from .views import UserCreateView, UserListView
 
 urlpatterns = [
-    # add your paths here
+    path('users/', UserListView.as_view(), name='user-list'),  # Admin only, list of users
+    path('register/', UserCreateView.as_view(), name='user-create'),  # Open registration
 ]
