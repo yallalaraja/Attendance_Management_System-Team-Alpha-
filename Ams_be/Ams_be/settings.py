de0b64e74f543ead7376587a98dbc538a4e0f49a
yallalarajareddy <yallalarajareddy1@gmail.com>
2025-05-10 09:58:59 +0530

    jwt expiry time increased

--- Ams_be/Ams_be/settings.py
+++ Ams_be/Ams_be/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,4 +155,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),  # or hours, minutes, etc.
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
